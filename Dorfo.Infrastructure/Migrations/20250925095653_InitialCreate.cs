82f933faf994a623c5a1bf923205488d52e3e7e0
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-09-25 17:02:27 +0700

    fix database

---
+++ Dorfo.Infrastructure/Migrations/20250925095653_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dorfo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "Merchants",
+                type: "character varying(1000)", // ✅ Postgres varchar
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "MenuItems",
+                type: "character varying(1000)", // ✅ Postgres varchar
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "Merchants");
+
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "MenuItems");
+        }
+    }
+}
