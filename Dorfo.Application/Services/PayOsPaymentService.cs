e87cca4010cc110e55604eb57fe052bfcdbe4a44
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-10-22 11:07:07 +0700

    them delivery address paymentos

--- Dorfo.Application/Services/PayOsPaymentService.cs
+++ Dorfo.Application/Services/PayOsPaymentService.cs
@@ -53,8 +53,8 @@ namespace Dorfo.Application.Services
         {
             var cart = await _redis.GetCartByMerchantAsync(userId, merchantId);
             if (cart == null) throw new Exception("Cart not found");
-            var user = await _unitOfWork.UserRepository.GetUserByIdAsync(userId);
-            var address = user.Addresses.FirstOrDefault(a => a.UserId == userId && a.IsDefault == true);
+            var address = await _unitOfWork.AddressRepository.GetAddressByUserId(userId);
+
             // 1. Tạo Order trong DB
             var orderRef = $"ORD-{DateTime.UtcNow:yyyyMMddHHmmss}-{Guid.NewGuid():N}".Substring(0, 24);
             var orderCode = DateTimeOffset.UtcNow.ToUnixTimeSeconds(); // long
@@ -72,7 +72,7 @@ namespace Dorfo.Application.Services
                 TotalAmount = cart.TotalAmount,
                 Status = OrderStatusEnum.WAITING_FOR_PAYMENT,
                 CreatedAt = DateTime.UtcNow,
-                DeliveryAddressId = address.AddressId,
+                DeliveryAddressId = address != null ? address.AddressId : null,
                 PaymentMethodId = 2, // PayOS
                 Items = cart.Items.Select(i => new OrderItem
                 {
